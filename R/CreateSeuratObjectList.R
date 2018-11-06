fcd9a0a04c02192947571c343adcbc7cebf349e7
jacobheng <jacob.heng@outlook.com>
2018-11-06 17:25:54 -0500

    Updated sparse matrix function

--- R/CreateSeuratObjectList.R
+++ R/CreateSeuratObjectList.R
@@ -14,21 +14,23 @@
 #' list <- CreateSeuratObjectList(exprs_matrix = exprs(dat), phenoData = pData(dat))
 
 
-CreateSeuratObjectList <- function(exprs_matrix, phenoData) {
+CreateSeuratObjectList <- function(exprs_matrix, phenoData, scaleData = FALSE) {
   sample_numbers <- unique(str_split_fixed(colnames(exprs_matrix), 
                                            pattern = "-", n = 2)[, 2])
   
-  SeuratObjectList <- lapply(sample_numbers, function(x) {
+  SeuratObjectList <- lapply(sample_numbers, function(x, scale) {
     exprs_subset <- exprs_matrix[ , str_split_fixed(colnames(exprs_matrix), 
                                          pattern = "-", n = 2)[, 2] == x]
+    exprs_subset <- as(as.matrix(exprs_subset), "sparseMatrix")
     phenoData_subset <- phenoData[str_split_fixed(rownames(phenoData), 
                                                   pattern = "-", n = 2)[, 2] == x,]
     SeuratObj <- CreateSeuratObject(raw.data = exprs_subset, meta.data=phenoData_subset)
     SeuratObj <- NormalizeData(SeuratObj)
     SeuratObj <- FindVariableGenes(SeuratObj, do.plot = F, display.progress = F)
+    if(scale == T) {
     SeuratObj <- ScaleData(SeuratObj)
-    
+    } else { SeuratObj <- SeuratObj }
     return(SeuratObj)
-  })
+  }, scale = scaleData)
   return(SeuratObjectList)
 }
