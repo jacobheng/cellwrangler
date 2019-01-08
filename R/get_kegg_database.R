1a2c99aef781a3f84a6093b1b5262b1e7996a25f
jacobheng <jacob.heng@outlook.com>
2019-01-08 18:22:55 -0500

    Created new database functions

---
+++ R/get_kegg_database.R
@@ -0,0 +1,26 @@
+#'Get kegg database for an organism
+#'
+#' get_kegg_database() is a wrapper function around the keggList() function that retrieves a kegg database
+#' for an organism and returns a dataframe
+#'
+#' @param database a KEGG database; one of options available via KEGGREST::listDatabases()
+#' @param organism KEGG organism code e.g. "mmu" for "Mus musculus"
+#' @keywords get_kegg_database
+#' @export
+#' @return a dataframe
+#' @examples
+#' kegg_db <- get_kegg_database("pathway","mmu")
+
+
+get_kegg_database <- function(database, organism) {
+  library(KEGGREST)
+  organism_ref <- as.data.frame((keggList("organism")))
+  species <- organism_ref[organism_ref$organism == organism,]$species
+  kegg_database <- keggList(database, organism)
+  kegg_database <- as.data.frame(cbind(kegg_database))
+  kegg_database$id <- str_split_fixed(rownames(kegg_database), ":", n =2)[,2]
+  kegg_database$species <- species
+  kegg_database[database] <- str_split_fixed(kegg_database[,1], paste(" - ", species, sep=""), n = 2)[,1]
+  return(kegg_database)
+  
+}
---
+++ R/get_kegg_pathway_genes.R
@@ -0,0 +1,20 @@
+#'Get genes for a kegg pathway
+#'
+#' get_kegg_pathway_genes() is a wrapper function around keggGet() that retrieves gene names in a KEGG pathway.
+#'
+#' @param pathway_name a KEGG pathway name
+#' @param kegg_pathway_df a kegg pathway database that is produced using the cellwrangler::get_kegg_database()
+#' function using "pathway" as an option.
+#' @keywords get_kegg_database
+#' @export
+#' @return a list
+#' @examples
+#' pathway_genes <- get_kegg_pathway_genes("Pentose phosphate pathway", mouse_kegg_pathways)
+
+get_kegg_pathway_genes <- function(pathway_name, kegg_pathway_df){
+  kegg_path_genes <- keggGet(kegg_pathway_df[kegg_pathway_df$pathway %in% pathway_name,]$id)[[1]]$GENE
+  kegg_path_genes <- cbind(kegg_path_genes)
+  kegg_path_genes <- str_split_fixed(kegg_path_genes[grep(";", kegg_path_genes),], ";", n=2)[,1]
+  kegg_path_genes <- as.vector(kegg_path_genes)
+  return(kegg_path_genes)
+}
