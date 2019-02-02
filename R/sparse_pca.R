ce6c1ef988de7840fa5861bb0f1cc2ba31a2a6d5
jacobheng <jacob.heng@outlook.com>
2019-02-02 10:02:05 -0500

    added new function

--- R/sparse_pca.R
+++ R/sparse_pca.R
@@ -1,13 +1,27 @@
+#'Maintain sparsity and run a fast PCA algorithm
+#'
+#' sparse_pca() runs a fast PCA algorithm on a sparse matrix. Adapted from CellRangerRkit.
+#'
+#' @param matrix a sparse matrix
+#' @param n_pcs number of principal components to compute
+#' @param mu column means
+#' @param s column standard deviations
+#' @param center_scale logical; whether to perform centering and scaling
+#' 
+#' @keywords sparse_pca()
+#' @export
+#' @return an irlba pca object
+#' @examples
+#' pca_object <- sparse_pca(exprs(dat), n_pcs = 40)
 
 
 
-
-sparse_pca <- function (x, n_pcs, mu = NULL, s = NULL, center_scale = TRUE) 
+sparse_pca <- function (matrix, n_pcs, mu = NULL, s = NULL, center_scale = TRUE) 
 {
   if (is.null(mu) && center_scale) 
-    mu <- colMeans(x)
+    mu <- colMeans(matrix)
   if (is.null(s) && center_scale) 
-    s <- apply(x, 2, sd, na.rm = TRUE)
+    s <- apply(matrix, 2, sd, na.rm = TRUE)
   irlba_wrapper <- function(...) {
     res <- try(irlba::irlba(fastpath = FALSE, ...), silent = T)
     if (inherits(res, "try-error")) {
@@ -17,15 +31,15 @@ sparse_pca <- function (x, n_pcs, mu = NULL, s = NULL, center_scale = TRUE)
   }
   if (center_scale) {
     s[s == 0] <- min(s[s > 0])
-    svd_res <- irlba_wrapper(x, n_pcs, center = mu, scale = s)
+    svd_res <- irlba_wrapper(matrix, n_pcs, center = mu, scale = s)
   }
   else {
-    svd_res <- irlba_wrapper(x, n_pcs)
+    svd_res <- irlba_wrapper(matrix, n_pcs)
   }
-  n <- dim(x)[1]
-  variance_sum <- sum(apply(x, 2, var, na.rm = TRUE)/(s^2))
+  n <- dim(matrix)[1]
+  variance_sum <- sum(apply(matrix, 2, var, na.rm = TRUE)/(s^2))
   var_pcs <- svd_res$d^2/(n - 1)/variance_sum
-  return(list(x = svd_res$u %*% diag(svd_res$d), rotation = svd_res$v, 
+  return(list(matrix = svd_res$u %*% diag(svd_res$d), rotation = svd_res$v, 
               sdev = svd_res$d/sqrt(n - 1), tot_var = variance_sum, 
               var_pcs = var_pcs))
 }
