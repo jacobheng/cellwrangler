d8e081c2888ac4322e6f7d34180f0202d8d497d2
jacobheng <jacob.heng@outlook.com>
2019-03-03 11:36:55 -0500

    Added new functions

--- R/calculate_mt_exprs.R
+++ R/calculate_mt_exprs.R
@@ -5,18 +5,19 @@
 #' @param cds monocle CellDataSet object
 #' @param genome genome to which transcripts are aligned to. Accepts either "mouse" or "human".
 #' @param return_prop logical; whether to return the value of mitochondrial transcripts as 
-#' a proportion of total transcripts for each cell; resulting output will be a dataframe. 
+#' a proportion of total transcripts for each cell; resulting output will be the input CDS with
+#' the values of mt_RNAs and mt_prop attached to the corresponding pData. 
 #' If FALSE, will return only absolute number of transcripts per cell.
 #' @keywords mt_genes, mt_RNAs
 #' @export
-#' @return A vector or a dataframe
+#' @return A vector or a cds (see above)
 #' @examples
 #' calculate_mt_exprs
 
 
 
 
-calculate_mt_exprs <- function(cds, genome = "mouse", return_prop = F) {
+calculate_mt_exprs <- function(cds, genome = "mouse", return_prop = T) {
 
   if(genome == "mouse") {
   mt_genes<- base::grepl("^mt-",fData(cds)$gene_short_name)
@@ -25,7 +26,7 @@ calculate_mt_exprs <- function(cds, genome = "mouse", return_prop = F) {
   mt_genes<- base::grepl("^MT-",fData(cds)$gene_short_name)
     } else { warning("Need to specify genome as human or mouse!") }
   }
-  if(return == F) {
+  if(return_prop == F) {
     mt_RNAs <- Matrix::colSums(exprs(cds[mt_genes,])) 
     tmp <- mt_RNAs
     names(tmp) <- rownames(pData(cds))
@@ -33,8 +34,9 @@ calculate_mt_exprs <- function(cds, genome = "mouse", return_prop = F) {
     mt_RNAs <- Matrix::colSums(exprs(cds[mt_genes,])) 
     Total_RNAs <- Matrix::colSums(exprs(cds))
     mt_prop <- mt_RNAs/Total_RNAs
-    tmp <- as.data.frame(cbind(mt_RNAs, mt_prop))
-    rownames(tmp) <- rownames(pData(cds))
+    tmp <- cds
+    pData(tmp)$mt_RNAs <- mt_RNAs
+    pData(tmp)$mt_prop <- mt_prop
   }
   
   return(tmp) 
