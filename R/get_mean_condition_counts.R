cd3d248ddf75fbe6421121836bdc2e7c6ccc58a7
jacobheng <jacob.heng@outlook.com>
2019-05-16 08:24:40 -0400

    Updated subset cds functions

--- R/get_mean_condition_counts.R
+++ R/get_mean_condition_counts.R
@@ -5,18 +5,19 @@
 #' 
 #' @param cds a CellDataSet object used in the monocle package
 #' @param condition column name in pData(cds) that specifies the condition for classifying cells e.g. genotype
+#' @param read_type string specifying type of reads in expression matrix; defaults to "UMIs"
 #' @keywords get_mean_condition_counts
 #' @export
 #' @return CellDataSet object
 #' @examples
 #' dat <- get_mean_condition_counts(dat, condition="genotype")
 
-get_mean_condition_counts <- function(cds, condition) {
+get_mean_condition_counts <- function(cds, condition, read_type = "UMIs") {
   tmp <- cds
   conditions <- unique(pData(cds)[,condition])
   
   for(i in 1:length(conditions)) {
-    fData(tmp)[paste0("Mean_", conditions[i], "_counts")] <- Matrix::rowSums(exprs(tmp[,pData(tmp)[,condition]==conditions[i]])) / sum(pData(tmp)[,condition] == conditions[i])
+    fData(tmp)[paste0("Mean_", conditions[i], "_", read_type)] <- Matrix::rowSums(exprs(tmp[,pData(tmp)[,condition]==conditions[i]])) / sum(pData(tmp)[,condition] == conditions[i])
   }
   return(tmp)
  
--- R/subset_cds.R
+++ R/subset_cds.R
@@ -7,6 +7,7 @@
 #' @param cds a CellDataSet object used in the monocle package
 #' @param group column name in pData(cds) specifying cell group that each cell belong to e.g. "CellType"
 #' @param min_expr expression threshold to use for detectGenes() function
+#' @param read_type string specifying type of reads in expression matrix; defaults to "UMIs"
 #' @keywords gene_barplot()
 #' @export
 #' @return CellDataSet object
@@ -14,7 +15,7 @@
 #' subset_cds("Rods", dat, group = "CellType", min_expr = 1)
 
 
-subset_cds <- function(cell_group, cds, group, min_expr=NULL) {
+subset_cds <- function(cell_group, cds, group, min_expr=NULL, read_type = "UMIs") {
   cds_subset <- cds[, pData(cds)[,group] == cell_group]
   cds_subset <- estimateSizeFactors(cds_subset)
   cds_subset <- estimateDispersions(cds_subset)
@@ -25,8 +26,8 @@ subset_cds <- function(cell_group, cds, group, min_expr=NULL) {
   cds_subset <- detectGenes(cds_subset, min_expr=min_expr)
   
   fData(cds_subset)[,paste0("Prop_", group, "_expressed")] <- Matrix::rowSums(exprs(cds_subset) > min_expr)/nrow(pData(cds_subset))
-  fData(cds_subset)[,paste0(group,"_total_UMIs_per_gene")] <- Matrix::rowSums(exprs(cds_subset))
-  fData(cds_subset)[,paste0("Mean_", group, "_UMIs")] <- Matrix::rowSums(exprs(cds_subset))/nrow(pData(cds_subset))
+  fData(cds_subset)[,paste0(group,"_total_", read_type, "_per_gene")] <- Matrix::rowSums(exprs(cds_subset))
+  fData(cds_subset)[,paste0("Mean_", group, "_", read_type)] <- Matrix::rowSums(exprs(cds_subset))/nrow(pData(cds_subset))
   fData(cds_subset)[,group] <- cell_group
   
   return(cds_subset)
